4d985f6ec39b2c371d6515f92f9c480b15ba7f07
Pat Mollins <pmollins@unb.ca>
2019-02-06 12:15:40 -0400

    Update tables.sql

--- SQL/tables.sql
+++ SQL/tables.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS characters(
     user    VARCHAR(255)    NOT NULL,
     name    VARCHAR(255)    NOT NULL,
     player  VARCHAR(255),
-    initiative  INT         NOT NULL,
+    health  INT,
     id      INT             NOT NULL    AUTO_INCREMENT,
     PRIMARY KEY(id),
     FOREIGN KEY(user) REFERENCES users(username)
@@ -19,5 +19,9 @@ CREATE TABLE IF NOT EXISTS characters(
 );
 
 CREATE TABLE IF NOT EXISTS monsters(
-
+    id      INT             NOT NULL,
+    name    VARCHAR(255)         NOT NULL,
+    health  INT             NOT NULL,
+    intitmod  INT           NOT NULL,
+    PRIMARY KEY(id)
 );
